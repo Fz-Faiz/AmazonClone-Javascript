6c2fd36d47d5aae1f31a91fd2aded3207ff3ab21
FaizAmazon <faizahmad43430@gmail.com>
2024-09-10 16:45:00 +0530

    save cart in local storage

--- data/cart.js
+++ data/cart.js
@@ -1,10 +1,13 @@
-export let cart=[{
+export let cart=JSON.parse(localStorage.getItem('cart'))  ||   [{
     productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
     quantity:2,
 },{
     productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
     quantity:1,
 }];
+function saveToStorage(){
+    localStorage.setItem('cart',JSON.stringify(cart));
+}
 export function addToCart(productId,quantity){
     let matchingItem;
     cart.forEach((cartItem) => {
@@ -20,6 +23,7 @@ export function addToCart(productId,quantity){
         quantity: 1
       });
     }
+    saveToStorage();
   
   }
 
@@ -31,5 +35,6 @@ export function addToCart(productId,quantity){
         }
     });
     cart=newCart;
+    saveToStorage();
 
   }
