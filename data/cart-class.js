c61f827505d5fa588c58376e147d86f33b21180c
FaizAmazon <faizahmad43430@gmail.com>
2024-09-20 19:38:39 +0530

    Make localStoragekey and loadFromStorage

--- data/cart-class.js
+++ data/cart-class.js
@@ -1,17 +1,18 @@
 class Cart{
-    cartItems  = undefined;
-    localStorageKey = undefined;
+    cartItems;
+    // private
+    #localStorageKey;
 
     constructor(localStorageKey){
         this.localStorageKey=localStorageKey;
-        this.loadFromStroage();
+        this.#loadFromStroage();
 
 
 
 
 
     }
-    loadFromStroage(){
+    #loadFromStroage(){
         this.cartItems=JSON.parse(localStorage.getItem(this.localStorageKey))  ||   [{
          productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
          quantity:2,
