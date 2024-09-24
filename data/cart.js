1f80faa898a747bd3892beac67cabaf66cc5fce2
FaizAmazon <faizahmad43430@gmail.com>
2024-09-24 22:27:10 +0530

    Lear promises and convert code to promises

--- data/cart.js
+++ data/cart.js
@@ -91,3 +91,14 @@ export function addToCart(productId,quantity=0){
 
 
   }
+  export function loadCart(fun = () => {}){
+    const xhr =new XMLHttpRequest();
+    xhr.addEventListener('load',()=>{
+      console.log(xhr.response)
+
+      fun();
+   
+    });
+    xhr.open('GET','https://supersimplebackend.dev/cart');
+    xhr.send();
+   }
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -4,8 +4,49 @@ import { renderOrderSummary
  //import '../data/cart-class.js';
  //import '../data/backend-practise.js';
  import { loadProducts } from "../data/products.js";
- 
-loadProducts(()=>{
+ import { loadCart } from "../data/cart.js";
+
+
+Promise.all([
+    new Promise((resolve)=>{
+        loadProducts(()=>{
+            resolve('value1');
+    
+        });   
+    }),
+    new Promise((resolve)=>{
+        loadCart(()=>{
+            resolve();
+        });
+
+    })
+
+]).then((values)=>{
+    console.log(values)
     renderOrderSummary();
     renderPaymentSummary();
+
 });
+
+/*
+
+new Promise((resolve)=>{
+    loadProducts(()=>{
+        resolve('value1');
+
+    });
+
+
+}).then((value)=>{
+    console.log(value);
+    return new Promise((resolve)=>{
+        loadCart(()=>{
+            resolve();
+        });
+
+    });
+
+}).then(()=>{
+  
+})
+    */
